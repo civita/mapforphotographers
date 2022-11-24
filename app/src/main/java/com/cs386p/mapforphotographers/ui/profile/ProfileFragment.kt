9be14b4b80dcacd7290af8b8608dccaf682522ca
civita <changlee@utexas.edu>
2022-11-24 14:41:25 -0800

    add auth

--- app/src/main/java/com/cs386p/mapforphotographers/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/cs386p/mapforphotographers/ui/profile/ProfileFragment.kt
@@ -1,13 +1,23 @@
 package com.cs386p.mapforphotographers.ui.profile
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.cs386p.mapforphotographers.AuthInit
 import com.cs386p.mapforphotographers.databinding.FragmentProfileBinding
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
 
@@ -16,6 +26,21 @@ class ProfileFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val viewModel: ProfileViewModel by viewModels()
+
+    private val signInLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            viewModel.updateUser()
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Log.d("MainActivity", "sign in failed ${result}")
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,8 +57,44 @@ class ProfileFragment : Fragment() {
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.buttonLogin.setOnClickListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            if(user == null) {
+                AuthInit(viewModel, signInLauncher)
+            } else {
+                viewModel.signOut()
+            }
+        }
+
+        viewModel.observeLoginButtonText().observe(viewLifecycleOwner) {
+            binding.buttonLogin.text = it
+        }
+
+        viewModel.observeDisplayName().observe(viewLifecycleOwner) {
+            binding.textWelcome.text = "Hi, " + it
+            binding.textEditUsername.setText(it)
+        }
+
+        binding.textWelcome.setOnClickListener {
+            binding.textWelcome.visibility = View.GONE
+            binding.textEditUsername.visibility = View.VISIBLE
+        }
+
+        binding.textEditUsername.setOnClickListener {
+            if (!binding.textEditUsername.text.isNullOrBlank()) {
+                AuthInit.setDisplayName(binding.textEditUsername.text.toString(), viewModel)
+                binding.textWelcome.visibility = View.VISIBLE
+                binding.textEditUsername.visibility = View.GONE
+            } else {
+                val toast = Toast.makeText(context, "Please provide a display name!", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/cs386p/mapforphotographers/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/cs386p/mapforphotographers/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,59 @@ package com.cs386p.mapforphotographers.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
 
 class ProfileViewModel : ViewModel() {
+
+    private var defaultDisplayName = "please login first!"
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is profile Fragment"
     }
     val text: LiveData<String> = _text
+
+
+    private var loginButtonText = MutableLiveData("Login") // default = login
+
+    fun observeLoginButtonText() : LiveData<String> {
+        return loginButtonText
+    }
+
+    // copied from fc7
+    private var displayName = MutableLiveData(defaultDisplayName)
+    private var email = MutableLiveData("Uninitialized")
+    private var uid = MutableLiveData("Uninitialized")
+
+    private fun userLogout() {
+        displayName.postValue(defaultDisplayName)
+        email.postValue("No email, no active user")
+        uid.postValue("No uid, no active user")
+        loginButtonText.postValue("Login")
+    }
+
+    fun updateUser() {
+        // XXX Write me. Update user data in view model
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user != null) {
+            displayName.postValue(user.displayName)
+            email.postValue(user.email)
+            uid.postValue(user.uid)
+            loginButtonText.postValue("Logout")
+        }
+    }
+
+    fun observeDisplayName() : LiveData<String> {
+        return displayName
+    }
+    fun observeEmail() : LiveData<String> {
+        return email
+    }
+    fun observeUid() : LiveData<String> {
+        return uid
+    }
+    fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        userLogout()
+    }
 }
