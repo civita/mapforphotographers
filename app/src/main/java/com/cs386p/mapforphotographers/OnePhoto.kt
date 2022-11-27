7fd5df6bbf921f89a8fb25f8c8e8abd8c5770334
civita <changlee@utexas.edu>
2022-11-26 20:48:14 -0800

    add map in onephoto

--- app/src/main/java/com/cs386p/mapforphotographers/OnePhoto.kt
+++ app/src/main/java/com/cs386p/mapforphotographers/OnePhoto.kt
@@ -1,15 +1,35 @@
 package com.cs386p.mapforphotographers
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.net.toFile
 import androidx.exifinterface.media.ExifInterface
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.cs386p.mapforphotographers.databinding.ActivityOnePhotoBinding
 import com.cs386p.mapforphotographers.model.PhotoMeta
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.IOException
 import java.io.InputStream
@@ -17,7 +37,7 @@ import java.net.URI
 import java.util.*
 
 
-class OnePhoto: AppCompatActivity() {
+class OnePhoto: AppCompatActivity(), OnMapReadyCallback {
     companion object {
         const val titleKey = "title"
         const val selfTextKey = "selfText"
@@ -33,13 +53,21 @@ class OnePhoto: AppCompatActivity() {
     private var thumbnailURL : String = """"""
     private var uri : String = """"""
     private val storage = Storage()
-    private var exif = arrayOf(String())
+
     private var photometa = PhotoMeta()
+
+    private lateinit var map: GoogleMap
+    private lateinit var geocoder: Geocoder
+    private var locationPermissionGranted = false
+    private val nearHarryRansomCenter = LatLng(30.284422165825934, -97.74120999100127)
+    
+    private var _binding: ActivityOnePhotoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val activityOnePhotoBinding = ActivityOnePhotoBinding.inflate(layoutInflater)
-        setContentView(activityOnePhotoBinding.root)
+        _binding = ActivityOnePhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         //setSupportActionBar(activityOnePostBinding.toolbar)
         //supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
@@ -48,18 +76,31 @@ class OnePhoto: AppCompatActivity() {
         uri = intent.getStringExtra(uriKey).toString()
         Log.d("xxx_onephoto", uri)
 
+        //google maps things...
+        checkGooglePlayServices()
+        requestPermission()
+        geocoder = Geocoder(applicationContext)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.one_photo_mapFrag) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         if (!uri.isNullOrEmpty()) {
 
-            activityOnePhotoBinding.onePhotoButtonSubmit.setOnClickListener {
-                if(!activityOnePhotoBinding.onePhotoTitle.text.isNullOrBlank()) {
+            binding.onePhotoButtonSubmit.setOnClickListener {
+                if(!binding.onePhotoTitle.text.isNullOrBlank()) {
+                    // todo: check gps is valid or not
                     //viewModel.pictureSuccess()
 //                    val localPhotoFile = File(Uri.parse(uri).toString())
 
                     val uuid = UUID.randomUUID().toString()
-                    photometa.pictureTitle = activityOnePhotoBinding.onePhotoTitle.text.toString()
-                    photometa.pictureDescription = activityOnePhotoBinding.onePhotoDescription.text.toString()
-                    storage.uploadImage(Uri.parse(uri), uuid) {
+                    photometa.pictureTitle = binding.onePhotoTitle.text.toString()
+                    photometa.pictureDescription = binding.onePhotoDescription.text.toString()
+                    storage.uploadImage(Uri.parse(uri), uuid) { ret ->
                         viewModel.createPhotoMeta(photometa, uuid)
+                        // todo snack not shown
+                        val snack = Snackbar.make(it,"Upload completed!", Snackbar.LENGTH_LONG)
+                        snack.show()
+                        finish()
                     }
                 } else {
                     val snack = Snackbar.make(it,"Please provide a title!", Snackbar.LENGTH_LONG)
@@ -67,7 +108,7 @@ class OnePhoto: AppCompatActivity() {
                 }
             }
 
-            activityOnePhotoBinding.onePhotoImage.setImageURI(Uri.parse(uri))
+            binding.onePhotoImage.setImageURI(Uri.parse(uri))
 
             val sIn: InputStream?
             try {
@@ -77,31 +118,33 @@ class OnePhoto: AppCompatActivity() {
 
                     Log.d("xxx_onephoto", exifInterface.toString())
                     photometa.pictureDate = exifInterface.getAttribute(ExifInterface.TAG_DATETIME).toString()
-                    activityOnePhotoBinding.onePhotoTime.text = photometa.pictureDate
+                    binding.onePhotoTime.text = photometa.pictureDate
 
                     photometa.pictureCamera = exifInterface.getAttribute(ExifInterface.TAG_MODEL).toString()
-                    activityOnePhotoBinding.onePhotoCamera.text = photometa.pictureCamera
+                    binding.onePhotoCamera.text = photometa.pictureCamera
 
                     photometa.pictureLens = exifInterface.getAttribute(ExifInterface.TAG_LENS_MAKE).toString()
-                    activityOnePhotoBinding.onePhotoLens.text = photometa.pictureLens
+                    binding.onePhotoLens.text = photometa.pictureLens
 
                     photometa.pictureShutterSpeed = exifInterface.getAttributeDouble(ExifInterface.TAG_SHUTTER_SPEED_VALUE, 0.00).toString()
-                    activityOnePhotoBinding.onePhotoShutterSpeed.text = photometa.pictureShutterSpeed
+                    binding.onePhotoShutterSpeed.text = photometa.pictureShutterSpeed
 
                     photometa.pictureFocalLength = exifInterface.getAttributeDouble(ExifInterface.TAG_FOCAL_LENGTH, 0.00).toString()
-                    activityOnePhotoBinding.onePhotoFocalLength.text = photometa.pictureFocalLength
+                    binding.onePhotoFocalLength.text = photometa.pictureFocalLength
 
                     photometa.pictureAperture = exifInterface.getAttributeDouble(ExifInterface.TAG_APERTURE_VALUE, 0.00).toString()
-                    activityOnePhotoBinding.onePhotoAperture.text = photometa.pictureAperture
+                    binding.onePhotoAperture.text = photometa.pictureAperture
 
                     photometa.pictureIso = exifInterface.getAttribute(ExifInterface.TAG_ISO_SPEED).toString() // todo: null?
-                    activityOnePhotoBinding.onePhotoIso.text = photometa.pictureIso
+                    binding.onePhotoIso.text = photometa.pictureIso
 
-                    photometa.pictureLat = exifInterface.getAttributeDouble(ExifInterface.TAG_GPS_LATITUDE, 0.00).toString()
-                    activityOnePhotoBinding.onePhotoLat.text = photometa.pictureLat // todo :covert
+                    //photometa.pictureLat = exifInterface.getAttributeDouble(ExifInterface.TAG_GPS_LATITUDE, 0.00).toString()
+                    photometa.pictureLat = exifInterface.latLong?.first().toString()
+                    binding.onePhotoLat.text = photometa.pictureLat
 
-                    photometa.pictureLng = exifInterface.getAttributeDouble(ExifInterface.TAG_GPS_LONGITUDE, 0.00).toString()
-                    activityOnePhotoBinding.onePhotoLng.text = photometa.pictureLng
+                    //photometa.pictureLng = exifInterface.getAttributeDouble(ExifInterface.TAG_GPS_LONGITUDE, 0.00).toString()
+                    photometa.pictureLng = exifInterface.latLong?.last().toString()
+                    binding.onePhotoLng.text = photometa.pictureLng
 
 
                 }
@@ -145,4 +188,115 @@ class OnePhoto: AppCompatActivity() {
 //            }
 //        })
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        this.onMapReady(map)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        Log.d("xxx", locationPermissionGranted.toString())
+        if( locationPermissionGranted ) {
+            // XXX Write me.
+            // Note, we checked location permissions in requestPermission, but the compiler
+            // might complain about our not checking it.
+            val permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            Log.d("xxx", permission.toString())
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                if (!locationPermissionGranted) {
+                    Toast.makeText(this,
+                        "Unable to show location - permission required",
+                        Toast.LENGTH_LONG).show()
+                    return
+                }
+            }
+            map.isMyLocationEnabled = true
+            map.uiSettings.isMyLocationButtonEnabled = true
+
+        }
+
+        map.setOnMapClickListener {
+            map.clear()
+
+            val titleString = "%.3f".format(it.latitude) + " " + "%.3f".format(it.longitude)
+            map.addMarker(
+                MarkerOptions()
+                    .position(it)
+                    .title(titleString)
+            )
+
+
+            photometa.pictureLat = it.latitude.toString()
+            binding.onePhotoLat.text = photometa.pictureLat
+
+            photometa.pictureLng = it.longitude.toString()
+            binding.onePhotoLng.text = photometa.pictureLng
+        }
+//        map.setOnMapLongClickListener {
+//            map.clear()
+//        }
+        // todo: add marker and move camera
+
+        // Start the map at the Harry Ransom center
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(nearHarryRansomCenter, 15.0f))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(photometa.pictureLat.toDouble(), photometa.pictureLng.toDouble()), 15.0f))
+        val titleString = "%.3f".format(photometa.pictureLat.toDouble()) + " " + "%.3f".format(photometa.pictureLng.toDouble())
+        map.addMarker(
+            MarkerOptions()
+                .position(LatLng(photometa.pictureLat.toDouble(), photometa.pictureLng.toDouble()))
+                .title(titleString)
+        )
+
+
+    }
+
+    private fun checkGooglePlayServices() {
+        val googleApiAvailability = GoogleApiAvailability.getInstance()
+        val resultCode =
+            googleApiAvailability.isGooglePlayServicesAvailable(this)
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (googleApiAvailability.isUserResolvableError(resultCode)) {
+                googleApiAvailability.getErrorDialog(this, resultCode, 257)?.show()
+            } else {
+                Log.i(javaClass.simpleName,
+                    "This device must install Google Play Services.")
+                finish()
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    locationPermissionGranted = true;
+                } else -> {
+                Toast.makeText(this,
+                    "Unable to show location - permission required",
+                    Toast.LENGTH_LONG).show()
+            }
+            }
+        }
+        locationPermissionRequest.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+    }
+
+
 }
